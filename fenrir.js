562806d866f52b11c83d38b3b37fff06fa966e6e
LimeIvy <raimu10311221@yahoo.co.jp>
2025-06-19 10:23:31 +0900

    コメントの追加

--- fenrir.js
+++ fenrir.js
@@ -1,14 +1,18 @@
 const isValid = (s) => {
   let stack = [];
+  // 対応マップ
   const map = {
     "(": ")",
     "{": "}",
     "[": "]",
   };
   for (let i = 0; i < s.length; i++) {
+    // 開き括弧なら配列に追加
     if (Object.keys(map).includes(s[i])) {
       stack.push(s[i]);
-    } else if (Object.values(map).includes(s[i])) {
+    }
+    // 閉じ括弧なら、対応する開き括弧をチェック
+    else if (Object.values(map).includes(s[i])) {
       if (stack.length === 0 || map[stack.pop()] !== s[i]) {
         return false;
       }
